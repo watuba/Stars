14dbe8607aaca47095ea30fb30ffad7b63e446b6
Ben Haun <ben@Benjamins-MacBook-Pro.local>
2016-11-21 01:42:06 -0800

    first commit, stars work and tap on them prints "tap". About to try indexing stars so we know which one was tapped.

--- Stars/ViewController.swift
+++ Stars/ViewController.swift
@@ -12,14 +12,56 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let bounds = UIScreen.main.bounds
+        let width = bounds.size.width
+        let height = bounds.size.height
+        let numberOfStars = Int(height/2.5)
+        
+        for i in 1...numberOfStars {
+            
+            let star = UITextView()
+            //star.backgroundColor = UIColor.blue
+            
+            let randX = Int(arc4random_uniform(UInt32(width)))
+            let randY = Int(arc4random_uniform(UInt32(height)))
+            let randWidth = Int(arc4random_uniform(3))
+            let randHeight = Int(arc4random_uniform(3))
+            
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+            star.addGestureRecognizer(tapGesture)
+            
+            star.frame = CGRect(x: randX, y: randY, width: randWidth, height: randHeight)
+            
+            self.view.addSubview(star)
+            
+        }
+    }
+    
+    func handleTap(sender: UITapGestureRecognizer) {
+        print("tap")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
 
+/*
+@IBOutlet weak var myView: UIView!
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // 1. create a gesture recognizer (tap gesture)
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+    
+    // 2. add the gesture recognizer to a view
+    myView.addGestureRecognizer(tapGesture)
+}
+
+// 3. this method is called when a tap is recognized
+func handleTap(sender: UITapGestureRecognizer) {
+    print("tap")
+}
+}
+*/
