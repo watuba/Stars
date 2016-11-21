b02e0d8b57119eaef05c4d1675c6aad2a10bd909
Ben Haun <ben@Benjamins-MacBook-Pro.local>
2016-11-21 00:40:31 -0800

    Initial Commit

---
+++ Stars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Ben Haun on 11/21/16.
+//  Copyright © 2016 Ben Haun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
